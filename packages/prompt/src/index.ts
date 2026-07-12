55d1f38eda3ae4272a564b73c28bcbe9bc819e6f
Nyx Programa <nyx-programa@local>
2026-07-12 20:32:18 -0300

    feat(prompt): render and compose prompts

--- packages/prompt/src/index.ts
+++ packages/prompt/src/index.ts
@@ -1,3 +1,5 @@
+export { PromptComposer } from "./PromptComposer";
+export { PromptRenderer } from "./PromptRenderer";
 export { PromptRegistry } from "./PromptRegistry";
 export type {
   PromptSection,
