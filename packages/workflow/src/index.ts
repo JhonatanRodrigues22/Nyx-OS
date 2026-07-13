6e4cd1422b938db92f52ce33f5d6642140a30f85
Nyx Programa <nyx-programa@local>
2026-07-12 21:58:31 -0300

    feat(workflow): execute workflow steps with retry and branching

--- packages/workflow/src/index.ts
+++ packages/workflow/src/index.ts
@@ -1,6 +1,8 @@
 export { emitWorkflowEvent } from "./WorkflowEvents";
+export { WorkflowExecutor } from "./WorkflowExecutor";
 export { WorkflowRegistry } from "./WorkflowRegistry";
 export type { WorkflowEventPayload } from "./WorkflowEvents";
+export type { WorkflowExecutorOptions, WorkflowRunOptions } from "./WorkflowExecutor";
 export type {
   WorkflowContext,
   WorkflowDefinition,
